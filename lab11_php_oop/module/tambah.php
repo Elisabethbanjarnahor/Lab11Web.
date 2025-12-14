446c1f395c493285e1b6a657eca5827a7de6103a
Elisabethbanjarnahor <elisabethmarbun34@gmail.com>
2025-12-14 14:29:44 +0700

    Update tambah.php

--- lab11_php_oop/module/tambah.php
+++ lab11_php_oop/module/tambah.php
@@ -1,28 +1,23 @@
 <?php
-$db = new Database();
-$form = new Form('', 'Simpan');
+$form = new Form("", "Simpan Artikel");
 
 if ($_POST) {
-    $data = [
+    $db = new Database();
+    $db->insert('artikel', [
         'judul' => $_POST['judul'],
         'isi'   => $_POST['isi']
-    ];
+    ]);
 
-    if ($db->insert('artikel', $data)) {
-        header("Location: /lab11_php_oop/artikel");
-        exit;
-    } else {
-        echo "<p style='color:red'>Gagal menyimpan data</p>";
-    }
+    header("Location: /lab11_php_oop/index.php/artikel/index");
+    exit;
 }
 ?>
 
 <h3>Tambah Artikel</h3>
 
 <?php
-$form->addField('judul', 'Judul Artikel');
-$form->addField('isi', 'Isi Artikel', 'textarea');
+$form->addField("judul", "Judul Artikel");
+$form->addField("isi", "Isi Artikel", "textarea");
+
 $form->displayForm();
 ?>
-
-<a href="/lab11_php_oop/artikel">← Kembali</a>
